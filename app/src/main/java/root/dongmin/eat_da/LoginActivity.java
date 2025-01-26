3933aa53053bbfb02707d649b0f91ce0377b6712
roy1704 <roy1704@naver.com>
2025-01-26 23:50:25 +0900

    2차실험

--- app/src/main/java/root/dongmin/eat_da/LoginActivity.java
+++ app/src/main/java/root/dongmin/eat_da/LoginActivity.java
@@ -8,8 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 //push 되나연ㄷㄷㄷㄷ
-public class LoginActivity extends AppCompatActivity {
+//오옹 나이스 이제 코드 수정하고 싶지만 지금 가족 여행중이라서 시간 될때마다 고쳐나가겠습니다.
 
+
+
+
+public class LoginActivity extends AppCompatActivity {
+    int qwa = 5;
+    int qwaa = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
