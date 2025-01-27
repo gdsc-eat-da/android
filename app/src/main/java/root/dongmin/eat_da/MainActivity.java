93513d489aaa4605c133cd21b92091d43ce66963
Dong Min <focuspdm4383@naver.com>
2025-01-27 17:03:15 +0900

    카ë메라 테스트 완료

--- app/src/main/java/root/dongmin/eat_da/MainActivity.java
+++ app/src/main/java/root/dongmin/eat_da/MainActivity.java
@@ -1,5 +1,6 @@
 package root.dongmin.eat_da;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,8 +18,7 @@ import root.dongmin.eat_da.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
-
-import root.dongmin.eat_da.databinding.ActivityMainBinding;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,15 +29,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 뷰 바인딩 초기화
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // 툴바 설정
         setSupportActionBar(binding.toolbar);
 
+        // 네비게이션 컨트롤러 설정
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        // 플로팅 액션 버튼 클릭 이벤트
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,23 +50,30 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // 버튼 초기화 및 클릭 이벤트 추가
+        Button button = findViewById(R.id.btngotophoto);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // PhotoActivity로 이동
+                Intent intent = new Intent(MainActivity.this, PhotoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // 메뉴 인플레이트
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
